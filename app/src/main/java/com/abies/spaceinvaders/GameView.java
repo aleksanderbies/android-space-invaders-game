31e76e10861bd931872677d38e1c3b9077af0f1c
Aleksander Bies <aleksanderbies@gmail.com>
2021-02-21 15:51:13 +0100

    sounds, mute/unmute function

--- app/src/main/java/com/abies/spaceinvaders/GameView.java
+++ app/src/main/java/com/abies/spaceinvaders/GameView.java
@@ -1,8 +1,16 @@
 package com.abies.spaceinvaders;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
@@ -17,16 +25,38 @@ public class GameView extends SurfaceView implements Runnable {
     private Thread thread;
     private boolean isPlaying, isGameOver = false;
     private Background background1, background2;
-    public static int screenX, screenY;
+    public static int screenX, screenY, score = 0;
     public static float screenRatioX, screenRatioY;
     private Paint paint;
     private Enemies [] enemies;
+    private SharedPreferences preferences;
     private Random random;
+    private SoundPool soundPool;
     private List<Bullet> bullets;
+    private int sound;
+    private GameActivity activity;
     Rocket rocket;
 
-    public GameView(Context context, int screenX, int screenY) {
-        super(context);
+    public GameView(GameActivity activity, int screenX, int screenY) {
+        super(activity);
+
+        this.activity = activity;
+
+        preferences = activity.getSharedPreferences("game",Context.MODE_PRIVATE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+            AudioAttributes audioAttributes= new AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC).setUsage(AudioAttributes.USAGE_GAME).build();
+
+            soundPool = new SoundPool.Builder().setAudioAttributes(audioAttributes).build();
+
+
+        } else {
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+
+        }
+
+        sound = soundPool.load(activity, R.raw.shoot, 1);
 
         this.screenX = screenX;
         this.screenY = screenY;
@@ -42,6 +72,8 @@ public class GameView extends SurfaceView implements Runnable {
         background2.y = -1* (background2.backgroud.getHeight());
 
         paint = new Paint();
+        paint.setTextSize(128);
+        paint.setColor(Color.WHITE);
 
         enemies = new Enemies[4];
 
@@ -93,7 +125,8 @@ public class GameView extends SurfaceView implements Runnable {
                 if (Rect.intersects(enemy.getCollisonShape(), bullet.getCollisonShape())){
                     enemy.y = -500;
                     bullet.y = -500;
-                    enemy.wasShot = true;
+                    score++;
+                    enemy.x = random.nextInt(screenX- enemy.width);
                 }
             }
         }
@@ -104,12 +137,15 @@ public class GameView extends SurfaceView implements Runnable {
         for (Enemies enemy : enemies){
 
             enemy.y += enemy.speed;
+
+            if (enemy.y>screenY){
+                enemy.y = -500;
+                enemy.x = random.nextInt(screenX- enemy.width);
+                score -=2;
+            }
+
             if (enemy.x + enemy.width < 0){
 
-                if(!enemy.wasShot){
-                   isGameOver = true;
-                   return;
-                }
                 int bound = (int) (30 * screenRatioY);
                 enemy.speed = random.nextInt(bound);
                 if (enemy.speed < 10 * screenRatioY){
@@ -117,8 +153,6 @@ public class GameView extends SurfaceView implements Runnable {
                 }
                 enemy.y = random.nextInt(150) -  4 * enemy.height;
                 enemy.x = random.nextInt(screenX- enemy.width);
-
-                enemy.wasShot = false;
             }
             if (Rect.intersects(enemy.getCollisonShape(),rocket.getCollisonShape())){
                 isGameOver = true;
@@ -132,10 +166,20 @@ public class GameView extends SurfaceView implements Runnable {
             Canvas canvas = getHolder().lockCanvas();
             canvas.drawBitmap(background1.backgroud, background1.x, background1.y, paint);
             canvas.drawBitmap(background2.backgroud, background2.x, background2.y, paint);
+
+            for (Enemies enemy : enemies){
+                canvas.drawBitmap(enemy.getEnemy(), enemy.x, enemy.y, paint);
+            }
+
+            canvas.drawText(String.valueOf(score), 890 * screenRatioX, 150 * screenRatioY, paint );
+
             if (isGameOver){
                 isPlaying = false;
                 canvas.drawBitmap(rocket.getExplosion(), rocket.x, rocket.y, paint);
                 getHolder().unlockCanvasAndPost(canvas);
+                saveIfHighScore();
+                score = 0;
+                waitBeforeExit();
                 return;
             }
 
@@ -149,6 +193,26 @@ public class GameView extends SurfaceView implements Runnable {
 
             canvas.drawBitmap(rocket.getRocket(), rocket.x, rocket.y, paint);
             getHolder().unlockCanvasAndPost(canvas);
+        }
+
+    }
+
+    private void waitBeforeExit() {
+        try {
+            Thread.sleep(3000);
+            activity.startActivity(new Intent(activity, MainActivity.class));
+            activity.finish();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveIfHighScore() {
+
+        if (preferences.getInt("highscore", 0)<score) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("highscore",score);
+            editor.apply();
         }
 
     }
@@ -198,6 +262,11 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     public void newBullet() {
+
+        if (!preferences.getBoolean("isMute", false)){
+            soundPool.play(sound, 1, 1, 0, 0, 1 );
+        }
+
         Bullet bullet = new Bullet(getResources());
         bullet.x = rocket.x + (rocket.width/2)- (bullet.width/2);
         bullet.y = rocket.y;
--- app/src/main/java/com/abies/spaceinvaders/MainActivity.java
+++ app/src/main/java/com/abies/spaceinvaders/MainActivity.java
@@ -3,11 +3,16 @@ package com.abies.spaceinvaders;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isMute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+        TextView highScoreTxt = findViewById(R.id.highscoretext);
+
+        SharedPreferences preferences = getSharedPreferences("game", MODE_PRIVATE);
+        highScoreTxt.setText("High Score: "+preferences.getInt("highscore", 0));
+
+        isMute = preferences.getBoolean("isMute", false);
+
+        ImageView volumeCtrl=findViewById(R.id.volumeCtrl);
+
+        if (isMute){
+            volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_off_24);
+        }else{
+            volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_up_24);
+        }
+        volumeCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isMute = !isMute;
+                if (isMute){
+                    volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_off_24);
+                }else{
+                    volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_up_24);
+                }
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("isMute", isMute);
+                editor.apply();
             }
         });
     }
